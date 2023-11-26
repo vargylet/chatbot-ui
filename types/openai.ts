bc1dbc03181c4c3a532b754250a073b33c7d594a
vargylet <5398052+vargylet@users.noreply.github.com>
2023-11-26 09:09:32 +0100

    added support for GPT-3.5-Turbo-1106

--- types/openai.ts
+++ types/openai.ts
@@ -10,6 +10,7 @@ export interface OpenAIModel {
 export enum OpenAIModelID {
   GPT_3_5 = 'gpt-3.5-turbo',
   GPT_3_5_AZ = 'gpt-35-turbo',
+  GPT_3_5_TURBO_1106 = 'gpt-3.5-turbo-1106',
   GPT_4 = 'gpt-4',
   GPT_4_32K = 'gpt-4-32k',
   GPT_4_TURBO = 'gpt-4-1106-preview'
@@ -30,6 +31,12 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
     name: 'GPT-3.5',
     maxLength: 12000,
     tokenLimit: 4000,
+  },
+  [OpenAIModelID.GPT_3_5_TURBO_1106]: {
+    id: OpenAIModelID.GPT_3_5_TURBO_1106,
+    name: 'GPT-3.5-Turbo-1106',
+    maxLength: 48000,
+    tokenLimit: 16000,
   },
   [OpenAIModelID.GPT_4]: {
     id: OpenAIModelID.GPT_4,
